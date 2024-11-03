eaa8c3336c08837d0c4a5532239fb7e69a430f0b
Nikita <137696366+ez2say@users.noreply.github.com>
2024-11-03 19:34:44 +0300

    fix(GameBootstraper) rightly logic load scene

--- Assets/Dev/Scripts/_Core/GameBootstraper.cs
+++ Assets/Dev/Scripts/_Core/GameBootstraper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public enum IDScene {INIT=0, GAMEPLAY=1}
+public enum IDScene { INIT = 0, GAMEPLAY = 1 }
 
 public class GameBootstraper : MonoBehaviour
 {
@@ -13,13 +13,21 @@ public class GameBootstraper : MonoBehaviour
     {
         int indexScene = SceneManager.GetActiveScene().buildIndex;
 
-        if (indexScene != (int) IDScene.INIT && !IsLoad)
+        if (indexScene == (int)IDScene.INIT)
         {
-            SceneManager.LoadScene((int) IDScene.INIT);
+            IsLoad = true;
+            return;
+        }
+
+        if (indexScene == (int)IDScene.GAMEPLAY && !IsLoad)
+        {
+            SceneManager.LoadScene((int)IDScene.INIT);
 
             IsLoad = true;
 
             return;
         }
+
+        
     }
 }
